c0a7e9055eb4776751e6208f8848b7907fbcb5f4
Aarti21 <aartigadge@gmail.com>
2015-04-14 07:50:46 -0700

    Update Default.aspx.cs

--- Project/Project/Default.aspx.cs
+++ Project/Project/Default.aspx.cs
@@ -5,10 +5,24 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Project.Model;
+
 
 namespace FinalProject {
     public partial class Default : System.Web.UI.Page {
         protected void Page_Load(object sender, EventArgs e) {
+            protected void Page_Load(object sender, EventArgs e) 
+        {
+            Model.Project p = new Model.Project();
+            string h = p.showHighlights();
+            Label1.Text = h;
+        }
+        protected void Timer1_Tick(object sender, EventArgs e)
+        {
+            Model.Project p = new Model.Project();
+            string h = p.showHighlights();
+            Label1.Text = h;
+        }
             
         }
     }
